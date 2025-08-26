7090df2ddef0c0a074c2799bca7750df7c2a658e
Ustinix <ksuletters@yandex.ru>
2025-08-26 14:40:53 +0300

    feat: add error page

--- src/pages/ErrorPage.vue
+++ src/pages/ErrorPage.vue
@@ -1,7 +1,21 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div></div>
+  <div class="errorPage-container">
+    <h2>Страница не существует</h2>
+    <v-btn to="/" color="primary" size="large">
+      <v-icon start>mdi-cart-arrow-right</v-icon>
+      Вернуться на главную
+    </v-btn>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.errorPage-container {
+  margin: 0 auto;
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+</style>
--- src/router/router.ts
+++ src/router/router.ts
@@ -21,6 +21,9 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior: () => ({
+    top: 0,
+  }),
 });
 
 export default router;
