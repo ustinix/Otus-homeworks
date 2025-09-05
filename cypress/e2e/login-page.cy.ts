a69dce2dc5cdf0f8f0c1e3f244c2ae09fe5d7ff7
Ustinix <ksuletters@yandex.ru>
2025-09-05 14:19:10 +0300

    feat: test for login form

--- cypress/e2e/login-page.cy.ts
+++ cypress/e2e/login-page.cy.ts
@@ -1,1 +1,24 @@
+describe('Login page', () => {
+  it('should login user successfully', () => {
+    cy.visit('/login');
+    cy.get('[data-test="username"]').type('Ksenia');
+    cy.get('[data-test="email"]').type('user@example.com');
+    cy.get('[data-test="password"]').type('Password123@');
+    cy.get('[data-test="submit-btn"]').click();
 
+    cy.url().should('include', '/');
+    cy.get('[data-test="user-profile"]').should('be.visible').and('contain', 'Ksenia');
+  });
+
+  it('should login admin successfully', () => {
+    cy.visit('/login');
+    cy.get('[data-test="username"]').type('AdminUser');
+    cy.get('[data-test="email"]').type('admin@example.com');
+    cy.get('[data-test="password"]').type('Password123@');
+    cy.get('[data-test="admin"]').find('input[type="checkbox"]').click({ force: true });
+    cy.get('[data-test="submit-btn"]').click();
+
+    cy.url().should('include', '/admin');
+    cy.get('[data-test="user-profile"]').should('be.visible').and('contain', 'AdminUser');
+  });
+});
